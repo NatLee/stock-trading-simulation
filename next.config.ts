7ffc3572d3d84f5ec4a5bbefd0e17b650b83287b
NatLee <natlee.work@gmail.com>
2026-02-02 00:11:20 +0800

    Implement GitHub Pages deployment and update Next.js configuration
    - Added a GitHub Actions workflow for deploying the application to GitHub Pages on push to the main branch.
    - Updated next.config.ts to support GitHub Pages with conditional basePath and assetPrefix settings, ensuring proper routing and asset loading in the deployed environment.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? '/stock-trading-simulation' : '',
+  assetPrefix: isGithubActions ? '/stock-trading-simulation/' : '',
 };
 
 export default nextConfig;
